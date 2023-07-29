8f490c3b61736690ddc882c3fe0eb7d26f1e8ee8
Priti Thube <137392329+pritithube22@users.noreply.github.com>
2023-07-29 16:46:11 +0530

    Add files via upload

---
+++ Login Registration System Task1/src/main/java/com/example/registrationlogindemo/RegistrationLoginDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.registrationlogindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationLoginDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationLoginDemoApplication.class, args);
+		System.out.println("Success.......");
+		//  http://localhost:8080/register
+	}
+
+}
